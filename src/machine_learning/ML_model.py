4b1cd2aa6f870b07da988bf0c70729322f317cdc
Ryan Tang <tangryanz@yahoo.ca>
2021-01-24 16:34:36 -0500

    Updated public and private functions

--- src/machine_learning/ML_model.py
+++ src/machine_learning/ML_model.py
@@ -13,24 +13,14 @@ new_persons = {}
 new_num = 0
 
 
-# Converts dataframe fed to model into tensorflow dataset
-def df_to_dataset(dataframe):
-    dataframe = dataframe.copy()
-    labels = dataframe.pop('averageRating')
-    ds = tf.data.Dataset.from_tensor_slices((dataframe.to_dict(orient='list'), labels))
-    ds = ds.shuffle(buffer_size=len(dataframe))
-    ds = ds.batch(64)
-    return ds
-
-
 # Splits dataframe into training, validation, and test datasets
 def create_datasets(dataframe):
     print("Creating datasets...")
     train, test = train_test_split(dataframe, test_size=0.2)
     train, val = train_test_split(train, test_size=0.2)
-    train_ds = df_to_dataset(train)
-    val_ds = df_to_dataset(val)
-    test_ds = df_to_dataset(test)
+    train_ds = _df_to_dataset(train)
+    val_ds = _df_to_dataset(val)
+    test_ds = _df_to_dataset(test)
     print("Datasets created")
     return train_ds, val_ds, test_ds
 
@@ -72,44 +62,6 @@ def test_model(model, test_ds):
     print("Accuracy", accuracy)
 
 
-# Reads user input for personnel when predicting rating (e.g. director, writer, etc.)
-def get_personnel(names_df):
-    global new_persons
-    global new_num
-
-    personnel = []
-    person = input()
-    while person != 'done':
-        person_df = names_df.loc[names_df['primaryName'] == person]
-        # Check if person is new
-        if person_df.empty:
-            if person in new_persons:
-                personnel.append(new_persons[person])
-            else:
-                # Create new ID for new person
-                new_id = 'nm_new' + str(new_num)
-                new_persons[person] = new_id
-                personnel.append(new_id)
-                new_num += 1
-        else:
-            # Obtain existing ID for person
-            person_row = person_df.iloc[0]
-            person_id = person_row['nconst']
-            personnel.append(person_id)
-        person = input()
-    return personnel
-
-
-# Reads user input for genres when predicting rating
-def get_genres():
-    genres = []
-    genre = input()
-    while genre != 'done':
-        genres.append(genre)
-        genre = input()
-    return genres
-
-
 # Predicts rating for new movie
 def predict(model, names_df):
     print('')
@@ -122,22 +74,22 @@ def predict(model, names_df):
     while not quit_predict:
 
         print('Enter full names of directors (e.g. Christopher Nolan), each on their own line, then enter "done":')
-        directors = get_personnel(names_df)
+        directors = _get_personnel(names_df)
         print('')
         print('Enter full names of writers (e.g. Aaron Sorkin), each on their own line, then enter "done":')
-        writers = get_personnel(names_df)
+        writers = _get_personnel(names_df)
         print('')
         print('Enter full names of actors (e.g. Leonardo DiCaprio), each on their own line, then enter "done":')
-        actors = get_personnel(names_df)
+        actors = _get_personnel(names_df)
         print('')
         print('Enter full names of actresses (e.g. Margot Robbie), each on their own line, then enter "done":')
-        actresses = get_personnel(names_df)
+        actresses = _get_personnel(names_df)
         print('')
 
         personnel = directors + writers + actors + actresses
 
         print('Enter genres, each on its own line, then enter "done":')
-        genres = get_genres()
+        genres = _get_genres()
         print('')
 
         print('Enter movie release date (if unknown then enter -1):')
@@ -195,3 +147,51 @@ def predict(model, names_df):
         print('')
 
     print('Thanks for using IMDb Movie Rating Predictor!')
+
+
+# Converts dataframe fed to model into tensorflow dataset
+def _df_to_dataset(dataframe):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('averageRating')
+    ds = tf.data.Dataset.from_tensor_slices((dataframe.to_dict(orient='list'), labels))
+    ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(64)
+    return ds
+
+
+# Reads user input for personnel when predicting rating (e.g. director, writer, etc.)
+def _get_personnel(names_df):
+    global new_persons
+    global new_num
+
+    personnel = []
+    person = input()
+    while person != 'done':
+        person_df = names_df.loc[names_df['primaryName'] == person]
+        # Check if person is new
+        if person_df.empty:
+            if person in new_persons:
+                personnel.append(new_persons[person])
+            else:
+                # Create new ID for new person
+                new_id = 'nm_new' + str(new_num)
+                new_persons[person] = new_id
+                personnel.append(new_id)
+                new_num += 1
+        else:
+            # Obtain existing ID for person
+            person_row = person_df.iloc[0]
+            person_id = person_row['nconst']
+            personnel.append(person_id)
+        person = input()
+    return personnel
+
+
+# Reads user input for genres when predicting rating
+def _get_genres():
+    genres = []
+    genre = input()
+    while genre != 'done':
+        genres.append(genre)
+        genre = input()
+    return genres
